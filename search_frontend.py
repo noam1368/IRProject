575e17b81ee42952d7a3b21db4d0c767e624a0d7
Ofek-Lutzky <ofeklutzky@gmail.com>
2023-01-07 15:04:11 +0200

    a

--- search_frontend.py
+++ search_frontend.py
@@ -14,7 +14,7 @@ from inverted_index_gcp import MultiFileReader
 import inverted_index_gcp
 import pickle
 from inverted_index_gcp import InvertedIndex
-from traitlets.traitlets import Long
+# from traitlets.traitlets import Long
 ###################
 
 #work3
@@ -31,34 +31,70 @@ corpus_stopwords = ["category", "references", "also", "external", "links",
 all_stopwords = english_stopwords.union(corpus_stopwords) #this is containing all the stop words
 RE_WORD = re.compile(r"""[\#\@\w](['\-]?\w){2,24}""", re.UNICODE)
 
+########################################################
+#making local variable
+# bucket_name = 'indexes_bucket'
+# client = storage.Client()
+# bucket = client.bucket(bucket_name)
+# body index from bucket
+
+#read one index
+
+# #TEXT
+# index_src = "indexTexts.pkl"
+# blob_index = bucket.blob(f"{bucket_name}/{index_src}")
+# pickel_in = blob_index.download_as_string()
+# indexText = pickle.loads(pickel_in)
+#
+# #Title
+# index_src = "indexTitles.pkl"
+# blob_index = bucket.blob(f"{bucket_name}/{index_src}")
+# pickel_in = blob_index.download_as_string()
+# indexTitle = pickle.loads(pickel_in)
+#
+# #Anchor
+# index_src = "indexAnchors.pkl"
+# blob_index = bucket.blob(f"{bucket_name}/{index_src}")
+# pickel_in = blob_index.download_as_string()
+# indexAnchor = pickle.loads(pickel_in)
+########################################################
+
 #todo change the paths
 #this is the read of the indexes we made before in the storage of the cloud
-# indexTiltePath="./indexes_bucket/indexTitle.parquet/"
-# indexTextPath="./indexes_bucket/indexText.parquet/"
-# indexAnchorPath="./indexes_bucket/indexTextPath/" #todo
-# postingsGcpPath="./indexes_bucket/postings_gcp/"
-# indexPageViews = "./indexes_bucket/postings_gcp/" #todo
-indexTiltePathNoam="./indexes-my-proj/indexTitles.pkl/"
-indexTextPathNoam="./indexes-my-proj/indexTexts.pkl/"
-indexAnchorPathNoam="./indexes-my-proj/indexAnchors.pkl/" #todo
-dlPathNoam = './indexes-my-proj/dl.pkl'
+# indexTiltePathNoam="./indexes-my-proj/indexTitles.pkl/"
+# indexTextPathNoam="./indexes-my-proj/indexTexts.pkl/"
+# indexAnchorPathNoam="./indexes-my-proj/indexAnchors.pkl/" #todo
+# dlPathNoam = './indexes-my-proj/dl.pkl'
+# # postingsGcpPathNoam="./indexes-my-proj/postings_gcp/"
+# id_titlePathNoam="./indexes-my-proj/id_title/part-00000-93004c08-4631-4e8b-9f56-4def1ff49509-c000.csv.gz/"
+# page_views_path = "./indexes-my-proj/pageviews-202108-user.pkl"
+# page_rank_path = "./indexes-my-proj/page-rank/part-00000-88749e01-b371-4d47-8aa9-7fa2ed8b5932-c000.csv.gz"
+
+
+# indexTiltePathOfek="./indexes_bucket/indexTitles.pkl/"
+# indexTextPathOfek="./indexes_bucket/indexTexts.pkl/"
+# indexAnchorPathOfek="./indexes_bucket/indexAnchors.pkl/" #todo
+
+dir = "./indexes_bucket"
+dlPathOfek = './indexes_bucket/dl.pkl'
 # postingsGcpPathNoam="./indexes-my-proj/postings_gcp/"
-id_titlePathNoam="./indexes-my-proj/id_title/part-00000-93004c08-4631-4e8b-9f56-4def1ff49509-c000.csv.gz/"
-page_views_path = "./indexes-my-proj/pageviews-202108-user.pkl"
-page_rank_path = "./indexes-my-proj/page-rank/part-00000-88749e01-b371-4d47-8aa9-7fa2ed8b5932-c000.csv.gz"
+id_titlePathOfek="./indexes_bucket/id_title/part-00000-93004c08-4631-4e8b-9f56-4def1ff49509-c000.csv.gz/"
+page_views_path = "./indexes_bucket/pageviews-202108-user.pkl"
+page_rank_path = "./indexes_bucket/page-rank/part-00000-88749e01-b371-4d47-8aa9-7fa2ed8b5932-c000.csv.gz"
 
 
 
 # indexTitle= InvertedIndex.read_index(indexTiltePath, "index_title")
 # indexText=InvertedIndex.read_index(indexTextPath, "index")#todo
 # indexAnchor=InvertedIndex.read_index(indexAnchorPath, "index_anchor")
-indexTitle= InvertedIndex.read_index(indexTiltePathNoam, "indecTitles")
-indexText=InvertedIndex.read_index(indexTextPathNoam, "indexTexts")#todo
-indexAnchor=InvertedIndex.read_index(indexAnchorPathNoam, "indexAnchor")
+
+indexTitle= InvertedIndex.read_index(dir, "indexTitles")
+indexText=InvertedIndex.read_index(dir, "indexTexts")
+indexAnchor=InvertedIndex.read_index(dir, "indexAnchors")
 
 
 
-f= gzip.open(id_titlePathNoam, 'rb')
+f= gzip.open(id_titlePathOfek, 'rb')
 titles = f.read()
 
 
@@ -68,7 +104,7 @@ with open(page_views_path, 'rb') as f:
 fi= gzip.open(page_rank_path, 'rb')
 page_rank = fi.read()
 
-with open(dlPathNoam, 'rb') as f:
+with open(dlPathOfek, 'rb') as f:
   DL = pickle.loads(f.read())
 
 
@@ -214,8 +250,8 @@ def search():
     #we are doing the assumption that the search on the text is inuf to tell what is the best 100
     ##todo maybe think about weight to the title and text like ass 4
     tokens_after_filter = [token for token in tokens if token in indexText.df[token] < 250000 and indexText.df] #todo change the number according to number we see warking good
-    bestDocs = query_get_top_N_tfidf(indexText,tokens_after_filter,100)
-    res = get_docs_title_by_id(bestDocs)
+    # bestDocs = query_get_top_N_tfidf(indexText,tokens_after_filter,100)
+    # res = get_docs_title_by_id(bestDocs)
     # END SOLUTION
     return jsonify(res)
 
@@ -242,8 +278,8 @@ def search_body():
     # BEGIN SOLUTION
     tokens = tokenize(query.lower())
     tokens_after_filter = [token for token in tokens if token in indexText.df[token] < 250000 and indexText.df] #todo change the number according to number we see warking good
-    bestDocs = query_get_top_N_tfidf(indexText,tokens_after_filter,100)
-    res = get_docs_title_by_id(bestDocs)
+    # bestDocs = query_get_top_N_tfidf(indexText,tokens_after_filter,100)
+    # res = get_docs_title_by_id(bestDocs)
     # END SOLUTION
     return jsonify(res)
 
@@ -270,9 +306,9 @@ def search_title():
       return jsonify(res)
     # BEGIN SOLUTION
     tokens = tokenize(query.lower())
-    bestDocs = query_get_tfidf_for_all_Title(indexTitle,tokens) #here we don't want to filter the tokens becuase the titles are small not like text
-    bestDocs.sort(key = lambda x:x[1], reverse = True)
-    res = bestDocs[:100]
+    # bestDocs = query_get_tfidf_for_all_Title(indexTitle,tokens) #here we don't want to filter the tokens becuase the titles are small not like text
+    # bestDocs.sort(key = lambda x:x[1], reverse = True)
+    # res = bestDocs[:100]
     # END SOLUTION
     return jsonify(res)
 
